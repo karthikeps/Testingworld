6019fc9a7c7fa5d8aefca91343b6e8db6bb04603
karthikeps <karthik.krishna1292@gmail.com>
2024-07-22 16:01:23 +0530

    Initial commit

---
+++ BasicPython.py
@@ -0,0 +1,9 @@
+print("Hello")
+print("This is my first program")
+a=(int(input("Enter the first number")))
+b=(int(input("Enter the second number")))
+print(a+b)
+fact=1
+for i in range(5,1,-1):
+    fact=fact*i
+print(fact)
---
+++ Variables.py
@@ -0,0 +1,8 @@
+com="zucitech"
+name="Karthik"
+print("my company is ",com,"and name is ",name)
+age=input("Enter your age : ")
+age=int(age)+10
+age=str(age)
+print("Age is "+age)
+print(type(age))
